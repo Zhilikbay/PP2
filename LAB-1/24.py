3fa1d272a2c5d5b97f847939ef4be0565407a803
Zhilikbay <a_zhilikbay@kbtu.kz>
2023-01-26 11:55:17 +0600

    First LAB WORK

---
+++ LAB-1/24.py
@@ -0,0 +1,14 @@
+print("Hello")
+print('Hello')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+b = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(b)
